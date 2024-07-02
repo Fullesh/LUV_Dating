56550b9464c9cdcbb20193deeed6c008dc0c7bca
Danila Belov <danilabelov.adm@yandex.ru>
2024-07-02 20:24:54 +0300

    Изменения в приложениях + миграции

--- LUV/settings.py
+++ LUV/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'LUV.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'luv_database',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -124,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    phone = models.CharField(max_length= 12, unique=True, verbose_name='Телефон')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    patronymic = models.CharField(max_length=50, verbose_name='Отчество', **NULLABLE)
+    is_verified = models.BooleanField(default=False, verbose_name='Подтверждён')
+    token = models.CharField(max_length=10, verbose_name='Токен', **NULLABLE)
+
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.first_name} - {self.phone} - {self.is_verified} - {self.token}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
